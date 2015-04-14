49c75923609e214f4186b147a72c1bc1d0af480a
mir0y <mirror.pace@gmail.com>
2015-04-14 23:28:54 +0800

    created sqlite in main~

--- app/src/main/java/com/example/smyconan/megamind/MainActivity.java
+++ app/src/main/java/com/example/smyconan/megamind/MainActivity.java
@@ -65,8 +65,13 @@ public class MainActivity extends Activity implements ImgListener {
         db dbtest = new db(this);
         SQLiteDatabase dbwrite = dbtest.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put("fname","嚓嚓嚓");
-        cv.put("md5", "asdfalsdfjiuohgoa");
+        cv.put("fname","手机管家广告");
+        cv.put("fmd5", "030ba16df0a060e3815392a4e4e22d63");
+        dbwrite.insert("user",null,cv);
+
+        cv = new ContentValues();
+        cv.put("fname","腾讯读书广告");
+        cv.put("fmd5", "a9a58b59625b7d413c2a0348efd41ab4");
         dbwrite.insert("user",null,cv);
 
         initMainUI();
